a8a7aabdea50e069432115d70c96c4ccf4d44b0a
taeism <taeism@gmail.com>
2020-05-27 12:21:33 -0400

    added things to forecast, changed weatherstat to weatherstack

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,7 @@ const forecast = (lat, lon, callback) => {
         } else if (body.error) {
             callback('Incorrect coordinate format', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out.' + 'Humidity is ' + body.current.humidity + '. The time observed is ' + body.current.observation_time + '.')
         }
     })
 }
